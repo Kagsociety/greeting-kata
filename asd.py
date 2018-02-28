b463581d5ae6466a6c616687cb262143280db3a8
“Diák <diak.dezso@gmail.com>
2018-02-28 10:51:35 +0100

    Elkódolgatok

--- asd.py
+++ asd.py
@@ -1,4 +1,13 @@
-a = "Sasha"
+def keret():
+	s = '*'
+	n = 80
+	result = ''.join([char*n for char in s])
+	return result
+a = raw_input("give me a name pls \n")
+if(a == ""):
+	a = "my friend"
 def greeting(a):
+	print keret()
 	print("Happy Eastern," + a)
+	print keret()
 print greeting(a)
